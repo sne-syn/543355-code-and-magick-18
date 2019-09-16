386bed8d9723f4e01dc22204fe47608ad1c948e2
sne <snezhana.syngur@gmail.com>
2019-09-16 11:36:26 +0200

    Вызывает пустое облако статистики

---
+++ js/stat.js
@@ -0,0 +1,17 @@
+window.renderStatistics = function (ctx) {
+  ctx.fillStyle = 'rgba(26, 41, 65, 0.9)';
+  ctx.fillRect(110, 18, 430, 270);
+
+  ctx.strokeStyle = 'rgba(26, 41, 65)';
+  ctx.strokeRect(110, 18, 430, 270);
+
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(100, 8, 430, 270);
+
+  ctx.strokeStyle = 'rgba(26, 41, 65)';
+  ctx.strokeRect(100, 8, 430, 270);
+}
+
+
+var players = [];
+console.log(players);
