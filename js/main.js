2103f2f2da2b1471e4ec2b46cfc413a5696513dc
sne <snezhana.syngur@gmail.com>
2019-10-18 18:27:22 +0200

    Работает с сетью

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// 'use strict';
+
+// (function () {
+//   var onError = function (message) {
+//     console.error(message);
+//   };
+
+//   var onSuccess = function (data) {
+//     console.log(data);
+//   };
+
+//   // window.load('https://js.dump.academy/code-and-magick/data', onSuccess, onError);
+
+// })();
