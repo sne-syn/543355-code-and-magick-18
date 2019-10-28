a8b4baac5ae1957426321a32a21fc04e16aab0a3
sne <snezhana.syngur@gmail.com>
2019-10-28 07:38:58 +0100

    Исправляет алгоритм

--- js/similar.js
+++ js/similar.js
@@ -7,37 +7,30 @@
 
   var getRank = function (wizard) {
     var rank = 0;
-
+    if (wizard.colorCoat === coatColor && wizard.colorEyes === eyesColor) {
+      rank += 3;
+    }
     if (wizard.colorCoat === coatColor) {
       rank += 2;
     }
     if (wizard.colorEyes === eyesColor) {
       rank += 1;
     }
+
     return rank;
   };
 
-  var namesComparator = function (left, right) {
-    if (left > right) {
-      return 1;
-    } else if (left < right) {
-      return -1;
-    } else {
-      return 0;
-    }
-  };
-
   var updateWizards = function () {
-    window.render(wizards.sort(function (left, right) {
+    window.render(wizards.slice().sort(function (left, right) {
       var rankDiff = getRank(right) - getRank(left);
       if (rankDiff === 0) {
-        rankDiff = namesComparator(left.name, right.name);
+        rankDiff = wizards.indexOf(left) - wizards.indexOf(right);
       }
       return rankDiff;
     }));
   };
 
-  window.wizard.coatEyesHandler = window.debounce(function (color) {
+  window.wizard.eyesChangeHandler = window.debounce(function (color) {
     eyesColor = color;
     updateWizards();
   });
